641418e96c49c52302a6e17a88630ed5477f886e
rym49 <hatanakaryoma.130@gmail.com>
2023-02-05 19:18:49 +0900

    空配列を受け取れるよう修正

--- infrastructure/file_system_store.go
+++ infrastructure/file_system_store.go
@@ -8,7 +8,12 @@ import (
 )
 
 func NewFileSystemPlayerStore(file *os.File) (*FileSystemPlayerStore, error) {
-	file.Seek(0, 0)
+	err := initializePlayerDBFile(file)
+
+	if err != nil {
+		return nil, fmt.Errorf("problem initializing player db file, %v", err)
+	}
+
 	league, err := domain.NewLeague(file)
 
 	if err != nil {
@@ -19,6 +24,23 @@ func NewFileSystemPlayerStore(file *os.File) (*FileSystemPlayerStore, error) {
 		database: json.NewEncoder(&tape{file}),
 		league:   league,
 	}, nil
+}
+
+func initializePlayerDBFile(file *os.File) error {
+	file.Seek(0, 0)
+
+	info, err := file.Stat()
+
+	if err != nil {
+		return fmt.Errorf("problem getting file info from file %s, %v", file.Name(), err)
+	}
+
+	if info.Size() == 0 {
+		file.Write([]byte("[]"))
+		file.Seek(0, 0)
+	}
+
+	return nil
 }
 
 type FileSystemPlayerStore struct {
