58b3a3efd6c29d6c70409db3d8ea43ab8ccc985b
rym49 <hatanakaryoma.130@gmail.com>
2023-02-05 16:12:06 +0900

    getLeagueが最初から読み直すように修正

--- infrastructure/file_system_store.go
+++ infrastructure/file_system_store.go
@@ -6,10 +6,11 @@ import (
 )
 
 type FileSystemPlayerStore struct {
-	database io.Reader
+	database io.ReadSeeker
 }
 
 func (f *FileSystemPlayerStore) GetLeague() []domain.Player {
+	f.database.Seek(0, 0)
 	league, _ := domain.NewLeague(f.database)
 	return league
 }
