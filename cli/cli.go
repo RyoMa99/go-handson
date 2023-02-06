9aea54ee7ba3d9bd9d6e484ab1acee20c13b3ef2
rym49 <hatanakaryoma.130@gmail.com>
2023-02-07 01:17:53 +0900

    ブラインドスケジュールの実装

--- cli/cli.go
+++ cli/cli.go
@@ -5,20 +5,40 @@ import (
 	"handson/domain"
 	"io"
 	"strings"
+	"time"
 )
 
-func NewCLI(store domain.PlayerStore, in io.Reader) *CLI {
-	return &CLI{store, bufio.NewScanner(in)}
+type BlindAlerter interface {
+	ScheduleAlertAt(duration time.Duration, amount int)
+}
+
+func NewCLI(store domain.PlayerStore, in io.Reader, alerter BlindAlerter) *CLI {
+	return &CLI{
+		playerStore: store,
+		in:          bufio.NewScanner(in),
+		alerter:     alerter,
+	}
 }
 
 type CLI struct {
 	playerStore domain.PlayerStore
 	in          *bufio.Scanner
+	alerter     BlindAlerter
 }
 
 func (cli *CLI) PlayPoker() {
+	cli.scheduleBlindAlerts()
 	userInput := cli.readLine()
 	cli.playerStore.RecordWin(extractWinner(userInput))
+}
+
+func (cli *CLI) scheduleBlindAlerts() {
+	blinds := []int{100, 200, 300, 400, 500, 600, 800, 1000, 2000, 4000, 8000}
+	blindTime := 0 * time.Second
+	for _, blind := range blinds {
+		cli.alerter.ScheduleAlertAt(blindTime, blind)
+		blindTime = blindTime + 10*time.Minute
+	}
 }
 
 func (cli *CLI) readLine() string {
