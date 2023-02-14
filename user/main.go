4f11277994e25ee44d579c8a89a45cf44e758e8e
rym49 <hatanakaryoma.130@gmail.com>
2023-02-15 02:02:54 +0900

    tcpサーバ起動処理

--- user/main.go
+++ user/main.go
@@ -1,4 +1,26 @@
 package main
 
+import (
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
 func main() {
+	log.Println("User Service")
+
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Println("ERROR:", err.Error())
+	}
+
+	s := grpc.NewServer()
+	// s.RegisterService()
+
+	log.Printf("Server started at %v", lis.Addr().String())
+	err = s.Serve(lis)
+	if err != nil {
+		log.Println("ERROR:", err.Error())
+	}
 }
